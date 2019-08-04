0840bcf0509c1d41b22709d4e7a32e46d3818f99
Kaiser Emon <kaisermdabu@gmail.com>
2019-08-04 10:04:03 +0600

    create a portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './modules/shared/pages/home/home.component';
+import { AboutComponent } from './modules/shared/pages/about/about.component';
+import { BlogComponent } from './modules/shared/pages/blog/blog.component';
+import { ContactComponent } from './modules/shared/pages/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
